672adb1286e1f65dceee2b2fafec5602466fce9d
idapass <info@idapass.com>
2020-12-01 03:16:03 -0800

    Fetch all the glassnode_data

--- src/glassnode-data/config.sh
+++ src/glassnode-data/config.sh
@@ -1,32 +1,9 @@
 # This is the config file for the crypto data fetcher (Glassnode API)
 
-API_KEY=""
-DATA_DIR="/data"
+API_KEY_FILE="/tmp/glassnode_api_key"
+API_KEY="$(cat "${API_KEY_FILE}")"
+DATA_DIR="./data"
 DOWNLOAD_DIR="./downloads"
+GLASSNODE_HOST="https://api.glassnode.com"
 
-endpoints_list="/v1/metrics/transactions/count
-/v1/metrics/indicators/stock_to_flow_deflection
-/v1/metrics/entities/sending_count
-/v1/metrics/indicators/reserve_risk
-/v1/metrics/indicators/unrealized_profit
-/v1/metrics/indicators/unrealized_loss
-/v1/metrics/entities/receiving_count
-/v1/metrics/indicators/puell_multiple
-/v1/metrics/market/price_usd_close
-/v1/metrics/blockchain/utxo_profit_relative
-/v1/metrics/market/mvrv
-/v1/metrics/transactions/transfers_volume_miners_to_exchanges
-/v1/metrics/market/mvrv_more_155
-/v1/metrics/transactions/transfers_volume_exchanges_net
-/v1/metrics/fees/fee_ratio_multiple
-/v1/metrics/distribution/balance_exchanges
-/v1/metrics/indicators/unrealized_profit_account_based
-/v1/metrics/entities/net_growth_count
-/v1/metrics/blockchain/block_size_mean
-/v1/metrics/blockchain/block_interval_mean
-/v1/metrics/indicators/cdd_supply_adjusted_binary
-/v1/metrics/indicators/asol
-/v1/metrics/addresses/min_100_count
-/v1/metrics/entities/active_count
-/v1/metrics/addresses/accumulation_balance"
-
+ENDPOINTS_LIST_FILE="endpoints_list"
--- src/glassnode-data/fetch_data.sh
+++ src/glassnode-data/fetch_data.sh
@@ -2,19 +2,52 @@
 
 source config.sh
 
-mkdir -p ${DATA_DIR}
+ep_category_filter="$"
 
-echo "Number of metrics to fetch: $(echo "$endpoints_list" | wc -l)"
+mkdir -p ${DATA_DIR}
+ENDPOINTS_LIST="$(cat "${ENDPOINTS_LIST_FILE}")"
+ep_len="$(echo "${ENDPOINTS_LIST}" | jq -r 'length')"
+ep_max="$(echo "${ENDPOINTS_LIST}" | jq -r 'length - 1')"
+echo "Downloading $ep_len metrics from Glassnode ..."
 
 count=0
 
-for endpoint in ${endpoints_list}; do
+for ep_index in $( seq 0 $ep_max ); do
 	echo "==================================================================================================================="
-	metric_name="$(echo $endpoint | awk -F'/' '{print $NF}')"
+	ep_path="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].path')"
+	ep_category="$(echo "$ep_path" | awk -F'/' '{print $4}')"
 
-	curl_cmd='curl -s "https://api.glassnode.com'${endpoint}'?a=BTC&i=24h&f=csv&api_key='${API_KEY}'" > '${DATA_DIR}/${metric_name}''
-	echo "${count}: ${metric_name} > ${DATA_DIR}/${metric_name}"
+	#Filter based on category name
+	[ "$(echo $ep_category | grep "$ep_category_filter")" == "" ] && echo "Skipping metric" && continue
 
+	ep_name="$(echo "$ep_path" | awk -F'metrics/' '{print $2}')"
+
+	# Set the ep_res based on ep_category
+	ep_res_list="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].resolutions')"
+	if [ "$(echo "$ep_category" | grep "supply")" != "" ]; then
+		ep_res="24h"
+	elif [ "$(echo "$ep_category" | grep "addresses")" != "" ]; then
+		ep_res="24h"
+	elif [ "$(echo "$ep_category" | grep "derivatives")" != "" ]; then
+		ep_res="24h"
+	else
+		if [ "$(echo "$ep_res_list" | grep 10m)" != "" ]; then
+			ep_res="10m"
+		elif [ "$(echo "$ep_res_list" | grep 1h)" != "" ]; then
+			ep_res="1h"
+		elif [ "$(echo "$ep_res_list" | grep 24h)" != "" ]; then
+			ep_res="24h"
+		fi
+	fi
+
+	ep_symbols="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].symbols')"
+
+	echo "$ep_index $ep_path $ep_res > ${DATA_DIR}/${ep_name}"
+
+	# create the category dir if it doesn't exists
+	[ ! -d "${DATA_DIR}/$ep_category" ] && mkdir -p ${DATA_DIR}/$ep_category
+
+	curl_cmd='curl -s "'${GLASSNODE_HOST}${ep_path}'?a=BTC&i='$ep_res'&f=csv&api_key='${API_KEY}'" > '${DATA_DIR}/${ep_name}''
 	eval "${curl_cmd}"
 	res=$?
 
