9a1dbd9b1423029aabc6a610cc32e72bc885c5bc
idapass <info@idapass.com>
2021-08-25 01:54:15 -0700

    Fetch the free version

--- src/glassnode-data/fetch_data.sh
+++ src/glassnode-data/fetch_data.sh
@@ -2,60 +2,75 @@
 
 source config.sh
 
-ep_category_filter="$"
+ep_category_filter=""
 
 mkdir -p ${DATA_DIR}
 ENDPOINTS_LIST="$(cat "${ENDPOINTS_LIST_FILE}")"
 ep_len="$(echo "${ENDPOINTS_LIST}" | jq -r 'length')"
 ep_max="$(echo "${ENDPOINTS_LIST}" | jq -r 'length - 1')"
+until=""
 echo "Downloading $ep_len metrics from Glassnode ..."
 
 count=0
 
+json_metrics="transfers_volume_miners_to_exchanges_all balance_exchanges_all balance_miners_all supply_distribution_relative"
+format="csv"
+
 for ep_index in $( seq 0 $ep_max ); do
 	echo "==================================================================================================================="
+        ep_tier="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].tier')"
+        echo $ep_tier
+        [ $ep_tier -ne 1 ] && continue
 	ep_path="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].path')"
 	ep_category="$(echo "$ep_path" | awk -F'/' '{print $4}')"
-
-	#Filter based on category name
-	[ "$(echo $ep_category | grep "$ep_category_filter")" == "" ] && echo "Skipping metric" && continue
-
 	ep_name="$(echo "$ep_path" | awk -F'metrics/' '{print $2}')"
+        ep_metric="$(echo $ep_name | awk -F/ '{print $2}')"
+	ep_symbol="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].symbol')"
+	ep_res_list="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].resolutions[]')"
+	ep_assets="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].assets[].symbol')"
 
-	# Set the ep_res based on ep_category
-	ep_res_list="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].resolutions')"
-	if [ "$(echo "$ep_category" | grep "supply")" != "" ]; then
-		ep_res="24h"
-	elif [ "$(echo "$ep_category" | grep "addresses")" != "" ]; then
-		ep_res="24h"
-	elif [ "$(echo "$ep_category" | grep "derivatives")" != "" ]; then
-		ep_res="24h"
-	else
-		if [ "$(echo "$ep_res_list" | grep 10m)" != "" ]; then
-			ep_res="10m"
-		elif [ "$(echo "$ep_res_list" | grep 1h)" != "" ]; then
-			ep_res="1h"
-		elif [ "$(echo "$ep_res_list" | grep 24h)" != "" ]; then
-			ep_res="24h"
-		fi
-	fi
+	if [ "$(grep "$ep_metric" <<< $json_metrics )" == "" ]; then
+	    format="csv"
+    else
+        format="json"
+    fi
 
-	ep_symbols="$(echo "${ENDPOINTS_LIST}" | jq -r '.['${ep_index}'].symbols')"
-
-	echo "$ep_index $ep_path $ep_res > ${DATA_DIR}/${ep_name}"
-
-	# create the category dir if it doesn't exists
+	# Create the category dir if it doesn't exists
 	[ ! -d "${DATA_DIR}/$ep_category" ] && mkdir -p ${DATA_DIR}/$ep_category
 
-	curl_cmd='curl -s "'${GLASSNODE_HOST}${ep_path}'?a=BTC&i='$ep_res'&f=csv&api_key='${API_KEY}'" > '${DATA_DIR}/${ep_name}''
-	eval "${curl_cmd}"
-	res=$?
+    for asset in BTC; do
 
-	if [ $res -ne 0 ]; then
-		echo "There was an issue while fetching the data for $endpoint"
-		echo "Try running this command manually:"
-		echo "${curl_cmd}"
-	fi
+        [ "$( grep $asset <<< $ep_assets )" == "" ] && continue
+        format=json
+        
+        for resolution in $ep_res_list; do
+            [ "$resolution" != "24h" ] && continue
+            echo $asset $resolution $format
+            curl_cmd='curl -s "'${GLASSNODE_HOST}${ep_path}'?a=${asset}&i='${resolution}'&s=1620086400&f=${format}&api_key='${API_KEY}'" > metric.tmp'
+            echo $curl_cmd
+            eval "${curl_cmd}"
+            res=$?
 
-	((count++))
+            if [ $res -ne 0 ]; then
+                echo "There was an issue while fetching the data for $endpoint"
+                echo "Try running this command manually:"
+                echo "${curl_cmd}"
+            fi
+
+            if [ "$(cat metric.tmp | grep forbidden)" == "" ]; then
+                echo "$ep_index $ep_path ${asset} $resolution > ${DATA_DIR}/${ep_name}_${asset}_${resolution}.txt"
+                jq -rc '.[] | "\(.t),\(.v)"' metric.tmp > met
+                cp met "${DATA_DIR}/${ep_name}_${asset}_${resolution}.txt"
+#                cp metric.tmp "${DATA_DIR}/${ep_name}_${asset}_${resolution}.txt"
+                ((count++))
+            else
+                echo "$ep_index $ep_path ${asset} $resolution: $(cat metric.tmp)"
+            fi
+            sleep 0.5
+        done
+    done
 done
+
+echo "============================================="
+echo "FINISHED FETCHING THE DATA FROM GLASSNODE.COM"
+echo "Number of fetched metrics: $count"
